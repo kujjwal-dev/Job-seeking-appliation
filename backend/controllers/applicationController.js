0c3817c0f93c03be9c1c37cc88663fdaddaee334
ujjwal <khannaujjwal21@gmail.com>
2024-07-01 13:37:27 +0530

    done frontend integration

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -60,80 +60,78 @@ export const jobSeekerDeleteApplication = catchAsyncError(async (req, res, next)
 export const postApplication = catchAsyncError(async (req, res, next) => {
     const { role } = req.user;
     if (role === "Employer") {
-        return next(new ErrorHandler("Employer is not allowed to access this resources", 400))
+      return next(
+        new ErrorHandler("Employer not allowed to access this resource.", 400)
+      );
     }
-
     if (!req.files || Object.keys(req.files).length === 0) {
-        return next(new ErrorHandler("Resume file Required"));
+      return next(new ErrorHandler("Resume File Required!", 400));
     }
-
+  
     const { resume } = req.files;
-    const allowedFormats = ["image/png", "image/jpg", "image/webp"];
+    const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
     if (!allowedFormats.includes(resume.mimetype)) {
-        return next(new ErrorHandler("Invalid file type.Please upload your resume in PNG,JPG OR WEBP format"))
-    };
-
+      return next(
+        new ErrorHandler("Invalid file type. Please upload a PNG file.", 400)
+      );
+    }
     const cloudinaryResponse = await cloudinary.uploader.upload(
-        resume.tempFilePath
+      resume.tempFilePath
     );
-
+  
     if (!cloudinaryResponse || cloudinaryResponse.error) {
-        console.error(
-            "Cloudinary Error",
-            cloudinaryResponse.error || "Unknown cloudinary Error"
-        );
-        return next(new ErrorHandler("Failed to upload resume", 500))
-    };
-
-    const { name, email, coverLetter, phone, address, jobID } = req.body;
-
+      console.error(
+        "Cloudinary Error:",
+        cloudinaryResponse.error || "Unknown Cloudinary error"
+      );
+      return next(new ErrorHandler("Failed to upload Resume to Cloudinary", 500));
+    }
+    const { name, email, coverLetter, phone, address, jobId } = req.body;
     const applicantID = {
-        user: req.user._id,
-        role: "Job Seeker"
+      user: req.user._id,
+      role: "Job Seeker",
     };
-
-    if (!jobID) {
-        return next(new ErrorHandler("Job not found", 404))
+    if (!jobId) {
+      return next(new ErrorHandler("Job not found!", 404));
     }
-
-    const jobDetails = await Job.findById(jobID);
+    const jobDetails = await Job.findById(jobId);
     if (!jobDetails) {
-        return next(new ErrorHandler("Job not found", 404))
+      return next(new ErrorHandler("Job not found!", 404));
     }
-
+  
     const employerID = {
-        user: jobDetails.postedBy,
-        role: "Employer"
+      user: jobDetails.postedBy,
+      role: "Employer",
     };
-
-    if (!name || !email || !coverLetter || !phone || !address || !applicantID || !employerID || !resume) {
-        return next(new ErrorHandler("Please fill all fields", 400))
+    if (
+      !name ||
+      !email ||
+      !coverLetter ||
+      !phone ||
+      !address ||
+      !applicantID ||
+      !employerID ||
+      !resume
+    ) {
+      return next(new ErrorHandler("Please fill all fields.", 400));
     }
-
     const application = await Application.create({
-        name,
-        email,
-        coverLetter,
-        phone,
-        address,
-        applicantID,
-        employerID,
-        resume: {
-            public_id: cloudinaryResponse.public_id,
-            url: cloudinaryResponse.secure_url,
-        },
+      name,
+      email,
+      coverLetter,
+      phone,
+      address,
+      applicantID,
+      employerID,
+      resume: {
+        public_id: cloudinaryResponse.public_id,
+        url: cloudinaryResponse.secure_url,
+      },
     });
-
     res.status(200).json({
-        success: true,
-        message: "Application Submitted!",
-        application,
+      success: true,
+      message: "Application Submitted!",
+      application,
     });
-
-
-
-
-})
-
-
-
+  });
+  
--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {deleteJob, getAllJobs, getmyJobs, postJob, updateJob} from "../controllers/jobController.js";
+import {deleteJob, getAllJobs, getSinglejob, getmyJobs, postJob, updateJob} from "../controllers/jobController.js";
 import {isAuthorized} from "../middleware/auth.js"
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post("/post", isAuthorized ,postJob)
 router.get("/getmyjobs", isAuthorized , getmyJobs )
 router.put("/update/:id", isAuthorized , updateJob )
 router.delete("/delete/:id", isAuthorized , deleteJob )
+router.get("/:id", isAuthorized, getSinglejob)
 
 export default router;
